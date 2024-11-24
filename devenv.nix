abf37131a3370cc24ada28b89e65a33a18d445ac
Vladimir Pouzanov <farcaller@gmail.com>
2024-11-24 13:01:09 +0000

    Add the devenv

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+  languages.java.enable = true;
+  languages.clojure.enable = true;
+  languages.javascript.enable = true;
+  languages.javascript.package = pkgs.nodejs;
+  dotenv.enable = true;
+  packages = with pkgs; [
+    babashka
+    protobuf
+    grpcurl
+    protoc-gen-js
+    protoc-gen-grpc-web
+    envoy
+    polylith
+  ];
+
+  services.redis.enable = true;
+  processes = {
+    
+  };
+}
