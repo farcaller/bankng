44b775a1e38508faa4f459c3e74633037cc0b927
Vladimir Pouzanov <farcaller@gmail.com>
2024-11-25 21:15:45 +0000

    Redirect from /login-2fa to /login if the login isn't complete.

--- projects/frontend/src/net/dracones/bankng/frontend/routes.cljs
+++ projects/frontend/src/net/dracones/bankng/frontend/routes.cljs
@@ -2,6 +2,7 @@
   (:require [re-frame.core :as rf]
             [reitit.frontend.easy :as rfe]
             [reitit.frontend :as reitit]
+            [reitit.frontend.controllers :as rfc]
             [net.dracones.bankng.frontend.login.views :as login]))
 
 (def routes
@@ -13,15 +14,23 @@
      :view login/login-page}]
    ["/login-2fa"
     {:name :login-2fa
-     :view login/login-otp-page}]])
+     :view login/login-otp-page
+     :controllers [{:start (fn [& params]
+                             (when-not @(rf/subscribe [:login/full-name])
+                               (rf/dispatch [::push-route :login])))}]}]])
 
 (rf/reg-event-db
  ::set-active-route
  (fn [db [_ match]]
    (assoc db :current-route match)))
 
-(defn navigate! [match _]
-  (rf/dispatch [::set-active-route match]))
+(defn navigate! [new-match _]
+  (let [old-match (rf/subscribe [::current-route])]
+    (println "navigate" old-match " -- " new-match)
+    (when new-match
+      (let [cs (rfc/apply-controllers (:controllers @old-match) new-match)
+            m  (assoc new-match :controllers cs)]
+        (rf/dispatch [::set-active-route m])))))
 
 (rf/reg-sub
  ::current-route
@@ -40,6 +49,12 @@
 
 (rf/reg-fx :push-route rfe/push-state)
 
+(rf/reg-event-fx
+ ::push-route
+ (fn [_ [_ name]]
+   {:push-route name}))
+
 (comment
   (reitit/match-by-name! (reitit/router routes) :home)
+  (rf/dispatch [::push-route :login-2fa])
   :rcf)
