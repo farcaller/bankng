d299900d02462e57f870ca851b7ce88960fb9397
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-01 17:46:38 +0000

    Dev repl goodies

---
+++ development/src/dev/repl.clj
@@ -0,0 +1,39 @@
+(ns dev.repl
+  (:require [playback.preload]
+            [mount.core :as mount]
+            [mount-up.core :as mu]
+            [taoensso.telemere :as log]
+            [clojure.tools.namespace.repl :as tn]))
+
+(defn stop []
+  (mount/stop))
+
+(defn start []
+  (mount/start))
+
+(defn refresh []
+  (stop)
+  (tn/refresh))
+
+(defn refresh-all []
+  (stop)
+  (tn/refresh-all))
+
+(defn go
+  "starts all states defined by defstate"
+  []
+  (start)
+  :ready)
+
+(defn reset
+  "stops all states defined by defstate, reloads modified source files, and restarts the states"
+  []
+  (stop)
+  (tn/refresh :after 'dev.repl/go))
+
+(mount/in-clj-mode)
+
+(defn setup! []
+  (log/set-min-level! :debug)
+  (mu/on-upndown :info mu/log :before)
+  (start))
