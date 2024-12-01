c5b3a9f8ad22f82107d3d5e306d6ee08f4049bff
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-01 19:26:07 +0000

    Questionably better frontend error reporting

--- components/frontend-service/src/net/dracones/bankng/frontend_service/core.clj
+++ components/frontend-service/src/net/dracones/bankng/frontend_service/core.clj
@@ -1,10 +1,18 @@
 (ns net.dracones.bankng.frontend-service.core
   (:require [net.dracones.bankng.pb-frontend.interface :as fpb]
-            [net.dracones.bankng.proto-tools.interface :refer [map->proto proto->map ->statusex]]
+            [net.dracones.bankng.proto-tools.interface :refer [map->proto proto->map ->status code? ex->status INTERNAL]]
             [net.dracones.bankng.pb-mucklet.interface :as mu]
             [net.dracones.bankng.codesender.interface :as co]
+            [net.dracones.bankng.log.interface :as log]
             [manifold.deferred :as d])
   (:import [net.dracones.bankng FirstFactorReply SecondFactorReply]))
+
+(defn return-error [err]
+  (let [st (->status err)
+        code (code? st)]
+    (if (= :UNKNOWN code)
+      (.asRuntimeException (ex->status INTERNAL err "Internal error. Please try again later."))
+      (.asRuntimeException st))))
 
 (defn on-first-factor [request responseObserver]
   (-> (d/let-flow [request (proto->map request)
@@ -16,7 +24,7 @@
                    _ (co/send-code (:char-id char))]
                   (.onNext responseObserver reply-pb)
                   (.onCompleted responseObserver))
-      (d/catch #(.onError responseObserver (->statusex %)))))
+      (d/catch #(.onError responseObserver (return-error %)))))
 
 (defn on-second-factor [request responseObserver])
 
--- components/proto-tools/src/net/dracones/bankng/proto_tools/interface.clj
+++ components/proto-tools/src/net/dracones/bankng/proto_tools/interface.clj
@@ -1,6 +1,6 @@
 (ns net.dracones.bankng.proto-tools.interface
   (:require [camel-snake-kebab.core :as csk])
-  (:import [io.grpc Status]))
+  (:import [io.grpc Status Status$Code]))
 
 (defn proto->map
   [proto-obj]
@@ -18,5 +18,42 @@
              `(~setter-name ~builder-sym ~field-value)))
        (.build ~builder-sym))))
 
-(defn ->statusex [th]
-  (-> (Status/fromThrowable th) (.asRuntimeException)))
+(defn ->status [th]
+  (Status/fromThrowable th))
+
+(defn ->statuserr [th]
+  (-> (->status th) (.asRuntimeException)))
+
+(defn ex->status [s th msg]
+  (-> (.withDescription s msg)
+      (.withCause th)))
+
+(defn code? [status]
+  (-> (.getCode status)
+      .name
+      keyword))
+
+(def ABORTED (Status/ABORTED))
+(def ALREADY_EXISTS (Status/ALREADY_EXISTS))
+(def CANCELLED (Status/CANCELLED))
+(def DATA_LOSS (Status/DATA_LOSS))
+(def DEADLINE_EXCEEDED (Status/DEADLINE_EXCEEDED))
+(def FAILED_PRECONDITION (Status/FAILED_PRECONDITION))
+(def INTERNAL (Status/INTERNAL))
+(def INVALID_ARGUMENT (Status/INVALID_ARGUMENT))
+(def NOT_FOUND (Status/NOT_FOUND))
+(def OK (Status/OK))
+(def OUT_OF_RANGE (Status/OUT_OF_RANGE))
+(def PERMISSION_DENIED (Status/PERMISSION_DENIED))
+(def RESOURCE_EXHAUSTED (Status/RESOURCE_EXHAUSTED))
+(def UNAUTHENTICATED (Status/UNAUTHENTICATED))
+(def UNAVAILABLE (Status/UNAVAILABLE))
+(def UNIMPLEMENTED (Status/UNIMPLEMENTED))
+(def UNKNOWN (Status/UNKNOWN))
+
+(comment
+  (-> (->status #>< _)
+      code?
+      .name
+      keyword)
+  :rcf)
