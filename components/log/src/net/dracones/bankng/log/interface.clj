15a85da2ef280ef8655e765664da83a7e74666b4
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-01 19:24:31 +0000

    An attempt to make logging more useful. It isn't

--- components/log/src/net/dracones/bankng/log/interface.clj
+++ components/log/src/net/dracones/bankng/log/interface.clj
@@ -3,4 +3,10 @@
 
 (defmacro debug [msg] (log/log! :debug msg))
 
-(defmacro info [msg] (log/log! :info msg))
+(defmacro info
+  ([msg] (log/log! :info msg))
+  ([msg dta] (log/log! {:level :info :data dta} msg)))
+
+(defmacro error
+  ([msg err] (log/log! {:error err} msg))
+  ([msg] (log/log! msg)))
