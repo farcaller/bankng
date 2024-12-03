2426cf7be44bd578507ee8e42cb83677478aaf18
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-03 17:53:34 +0000

    Add a bunch of db mocks

---
+++ components/xtdb-inproc/src/net/dracones/bankng/xtdb_inproc/mocks.clj
@@ -0,0 +1,43 @@
+(ns net.dracones.bankng.xtdb-inproc.mocks
+  (:require [net.dracones.bankng.xtdb.interface :as db]
+            [xtdb.api :as xt]
+            [net.dracones.bankng.accounts.interface :refer [calculate-iban]]))
+
+(defn account-id [iban]
+  (keyword "account" (calculate-iban iban)))
+
+(defn seed []
+  (xt/submit-tx
+   db/conn
+   [[::xt/put {:xt/id :currency/SCAM
+               :currency/name "SCAM"
+               :currency/symbol "₷"}]
+
+    [::xt/put {:xt/id :customer/cajd55e9gbrqf703lcvg
+               :customer/full-name "Shinyuu Wolfy"
+               :customer/accounts [(account-id 123)]}]
+
+    [::xt/put {:xt/id (account-id 123)
+               :account/name "Personal"
+               :account/currency :currency/SCAM
+               :account/balance 101}]
+    [::xt/put {:xt/id (account-id 789)
+               :acc/name "Business"
+               :acc/currency :currency/SCAM
+               :acc/balance 101}]]))
+
+(comment
+  (seed)
+  ; get a customer
+  (xt/pull (xt/db db/conn) '[*] :customer/cajd55e9gbrqf703lcvg)
+
+  ; get the account details
+  (xt/pull (xt/db db/conn) '[* {:account/currency [*]} {:customer/_accounts [:customer/full-name]}] (account-id 123))
+  
+  ; get the number of accounts per customer
+  (xt/q
+   (xt/db db/conn)
+   '{:find [(count ?ac)]
+     :where [[?c :customer/full-name "Shinyuu Wolfy"]
+             [?c :customer/accounts ?ac]]})
+  :rcf)
