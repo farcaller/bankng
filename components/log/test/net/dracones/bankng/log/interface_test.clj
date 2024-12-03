731288bd851d84e132f5d3cf1685735b8b8e897f
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-03 20:48:25 +0000

    Remove the log brick, it doesn't seem to work anyway

--- components/log/test/net/dracones/bankng/log/interface_test.clj
+++
@@ -1,6 +0,0 @@
-(ns net.dracones.bankng.log.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [net.dracones.bankng.log.interface :as log]))
-
-(deftest dummy-test
-  (is (= 1 1)))
