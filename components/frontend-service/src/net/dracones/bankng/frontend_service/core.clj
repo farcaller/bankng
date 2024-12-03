1abda31e5010278fe581929bfc67659c10056b41
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-03 17:56:22 +0000

    Add second factor flow

--- components/frontend-service/src/net/dracones/bankng/frontend_service/core.clj
+++ components/frontend-service/src/net/dracones/bankng/frontend_service/core.clj
@@ -3,11 +3,13 @@
             [net.dracones.bankng.proto-tools.interface :refer [map->proto proto->map ->status code? ex->status INTERNAL]]
             [net.dracones.bankng.pb-mucklet.interface :as mu]
             [net.dracones.bankng.codesender.interface :as co]
-            [net.dracones.bankng.log.interface :as log]
+            [net.dracones.bankng.jwt.interface :as jwt]
+            [taoensso.telemere :as log]
             [manifold.deferred :as d])
   (:import [net.dracones.bankng FirstFactorReply SecondFactorReply]))
 
 (defn return-error [err]
+  (log/error! err)
   (let [st (->status err)
         code (code? st)]
     (if (= :UNKNOWN code)
@@ -18,14 +20,26 @@
   (-> (d/let-flow [request (proto->map request)
                    full-name (:full-name request)
                    char (mu/lookup-character full-name)
+                   session-id (str (random-uuid))
                    reply-pb (map->proto FirstFactorReply
                                         {:first-name (:first-name char)
-                                         :pfp-url (:pfp-url char)})
-                   _ (co/send-code (:char-id char))]
+                                         :pfp-url (:pfp-url char)
+                                         :session-id session-id})
+                   _ (co/send-code session-id (:char-id char))]
                   (.onNext responseObserver reply-pb)
                   (.onCompleted responseObserver))
       (d/catch #(.onError responseObserver (return-error %)))))
 
-(defn on-second-factor [request responseObserver])
+(defn on-second-factor [request responseObserver]
+  (-> (d/let-flow [request (proto->map request)
+                   code (:code request)
+                   session-id (:session-id request)
+                   char-id (co/verify-code session-id code)
+                   jwt-str (jwt/create-jwt char-id)
+                   reply-pb (map->proto SecondFactorReply
+                                        {:jwt jwt-str})]
+                  (.onNext responseObserver reply-pb)
+                  (.onCompleted responseObserver))
+      (d/catch #(.onError responseObserver (return-error %)))))
 
 (defn create-service [] (fpb/auth-impl #'on-first-factor #'on-second-factor))
--- components/frontend-service/src/net/dracones/bankng/frontend_service/interface.clj
+++ components/frontend-service/src/net/dracones/bankng/frontend_service/interface.clj
@@ -1,4 +1,5 @@
 (ns net.dracones.bankng.frontend-service.interface
   (:require [net.dracones.bankng.frontend-service.core :as core]))
 
-(defn create-service [] (core/create-service))
+(defn create-service []
+  (core/create-service))
