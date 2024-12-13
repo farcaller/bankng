c6546541f5009ce0e5d3ebe9d5d715fed293ad84
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-13 11:27:53 +0000

    Expose xtdb apis through a component

--- components/xtdb-inproc/src/bankng/xtdb/ifc.clj
+++ components/xtdb-inproc/src/bankng/xtdb/ifc.clj
@@ -1,5 +1,9 @@
 (ns bankng.xtdb.ifc
   (:require [mount.core :refer [defstate]]
-            [bankng.xtdb-inproc.core :as core]))
+            [bankng.xtdb-inproc.core :as core]
+            [xtdb.api :as xt]))
 
 (defstate conn :start (core/start) :stop (core/stop conn))
+
+(defmacro pull [query edn]
+  `(xt/pull (xt/db conn) (quote ~query) ~edn))
