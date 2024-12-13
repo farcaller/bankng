f2dd81ff7c33faf03ed6a370bacaa4a3d4ac653f
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-13 20:08:10 +0000

    Add tracing to xtdb

--- components/xtdb-inproc/src/bankng/xtdb/ifc.clj
+++ components/xtdb-inproc/src/bankng/xtdb/ifc.clj
@@ -1,9 +1,13 @@
 (ns bankng.xtdb.ifc
   (:require [mount.core :refer [defstate]]
             [bankng.xtdb-inproc.core :as core]
-            [xtdb.api :as xt]))
+            [xtdb.api :as xt]
+            [taoensso.telemere :refer [trace!]]))
 
 (defstate conn :start (core/start) :stop (core/stop conn))
 
-(defmacro pull [query edn]
-  `(xt/pull (xt/db conn) (quote ~query) ~edn))
+(defmacro pull [query eid]
+  `(trace!
+    {:id :xtdb/pull
+     :otel/trace-attrs {:query (quote ~query) :eid ~eid}}
+    (xt/pull (xt/db conn) (quote ~query) ~eid)))
