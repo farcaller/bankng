ef9c317344140e3456956967cd64ace276f5f395
Vladimir Pouzanov <farcaller@gmail.com>
2024-12-28 19:19:03 +0000

    Format the routes file

--- components/web-routes/src/bankng/web_routes/ifc/events.cljs
+++ components/web-routes/src/bankng/web_routes/ifc/events.cljs
@@ -10,25 +10,25 @@
    :after
    (fn [context]
      (let [db (-> context :effects :db)
-                   route-match (:current-route db)
-                   auth (-> route-match :data :auth)]
-               (if-let [new-ctx (condp = auth
-                                  :required (when (nil? (-> db :login :jwt))
-                                              (assoc-in context [:effects :push-route] :login))
-                                  :required-login (when (nil? (-> db :login :full-name))
-                                                    (println "redirecting to login, no name" (:login db))
-                                                    (assoc-in context [:effects :push-route] :login))
-                                  nil)]
-                 new-ctx
-                 context)))))
+           route-match (:current-route db)
+           auth (-> route-match :data :auth)]
+       (if-let [new-ctx (condp = auth
+                          :required (when (nil? (-> db :login :jwt))
+                                      (assoc-in context [:effects :push-route] :login))
+                          :required-login (when (nil? (-> db :login :full-name))
+                                            (println "redirecting to login, no name" (:login db))
+                                            (assoc-in context [:effects :push-route] :login))
+                          nil)]
+         new-ctx
+         context)))))
 
 (rf/reg-event-db
  :routes/set-active-route
  [verify-auth accounts/fetch-accounts transfers/reset-source-account]
  (fn [db [_ match]]
    (-> db
-    (assoc :previous-route (:current-route db))
-    (assoc :current-route match))))
+       (assoc :previous-route (:current-route db))
+       (assoc :current-route match))))
 
 (rf/reg-event-fx
  :routes/push-route
